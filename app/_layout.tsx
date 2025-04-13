fa14dd1c7839e74facf146c9db98dd05fe395ee8
unknown <ansajid2002@gmail.com>
2025-04-13 19:11:14 +0530

    added android folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
 import "../global.css"
 import { SessionProvider } from '@/context/Authcontext';
 import { Slot } from 'expo-router';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Root() {
   // Set up the auth context and render our layout inside of it.
   return (
-    
+    <GestureHandlerRootView className="flex-1">
     <SessionProvider>
       <Slot />
     </SessionProvider>
+      </GestureHandlerRootView>
   );
 }
